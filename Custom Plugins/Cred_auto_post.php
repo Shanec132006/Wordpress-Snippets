9d81132674206c5ac0e4d75c4cf796fc82b4cd33
Kenrick Beckett <kenrick.beckett@gmail.com>
2015-02-06 09:57:26 -0500

    Update Cred_auto_post.php

--- Custom Plugins/Cred_auto_post.php
+++ Custom Plugins/Cred_auto_post.php
@@ -18,31 +18,28 @@
 add_action('cred_before_save_data', 'my_success_action',10,1);
 function my_success_action($form_data)
 {
-// if a specific form
-if ($form_data['id']==118)
- 
-{
-
-	$upload_dir = wp_upload_dir();
-	$image_path = $upload_dir['url'] ."/" . $_POST [ 'wpcf-cover-image'];
-    $image_value= '<img src='.$image_path.'>';
- 
-
-		// Create post object
-	$my_post = array(
-	  'post_title'    => 'New book is now available: '.$_POST['post_title'],
-	  'post_content'  => 'Description: '.$_POST['post_content'].' by: '.$_POST['authors'].'
-	  '.$image_value.'
-	  Summary: '.$_POST['wpcf-summary'].'
-	  # of Pages: '.$_POST['wpcf-number-of-page'].'
-	  Rating: '.$_POST['wpcf-rating']['value'],
-	  'post_status'   => 'publish',
-	  'post_author'   => 1,
-	  'post_category' => array(8,39)
-	);
-
-	// Insert the post into the database
-	wp_insert_post( $my_post );
-	}
+  // if a specific form
+  if ($form_data['id']==118)
+  {
+    $upload_dir = wp_upload_dir();
+  	$image_path = $upload_dir['url'] ."/" . $_POST [ 'wpcf-cover-image'];
+  	$image_value= '<img src='.$image_path.'>';
+   
+  	// Create post object
+  	$my_post = array(
+      'post_title'    => 'New book is now available: '.$_POST['post_title'],
+      'post_content'  => 'Description: '.$_POST['post_content'].' by: '.$_POST['authors'].'
+      '.$image_value.'
+      Summary: '.$_POST['wpcf-summary'].'
+      # of Pages: '.$_POST['wpcf-number-of-page'].'
+      Rating: '.$_POST['wpcf-rating']['value'],
+      'post_status'   => 'publish',
+      'post_author'   => 1,
+      'post_category' => array(8,39)
+      );
+      
+      // Insert the post into the database
+      wp_insert_post( $my_post );
+  }
 }
 ?>
